dc3b23a9c8dd7d5ec841593716e4723d82f2d28d
Bimal <bimalbhusal1st2075@gmail.com>
2025-02-18 17:54:18 +0545

    utils added

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is running at http://localhost:${port}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Bimal Mongodb connection failed and the error is: ",err);
+    
+})
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+;(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Bimal Error while connecting express app with DB and the error is: "+error);
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.error("Bimal Error while connecting with DB and the error is: "+error);
+        throw error;
+    }
+})();
+*/
